2d11be05f29fe43772c1b4e5f2988394c3309f1b
peter-spencer-ons <peter.spencer@ext.ons.gov.uk>
2025-05-09 11:59:14 +0000

    feat(backend): added soc_lookup. updated notebook with current example ussage

---
+++ src/occupational_classification/meta/classification_meta.py
@@ -0,0 +1,70 @@
+"""Module for the `ClassificationMeta` class.
+
+This module defines the `ClassificationMeta` class, which represents metadata
+for occupational classification codes. It includes attributes for code, group
+title, group description and qualifications and tasks lists, as well as methods
+for code matching and formatted output.
+
+Classes:
+    ClassificationMeta: A Pydantic model for occupational classification metadata.
+"""
+
+from typing import Optional  # list for list for tasks
+
+from pydantic import BaseModel, Field
+
+from occupational_classification.utils.constants import SOC_CODE_LENGTH
+
+
+class ClassificationMeta(BaseModel):
+    """Represents a classification meta model.
+
+    Attributes:
+        code (str): Category code. A code of length between 1 and 4 digits long.
+        soc2020_group_title (str): A short, general title of the group.
+        group_description (str): Provides description of the group with more
+            details than group_title.
+        qualifications (str): Applies only for Unit Group. Provides string that
+            represents typical entry routes and qualifications required.
+        tasks (str): Applies only for Unit Group. Provides a string that
+            represents a list of typical tasks for given unit group.
+    """
+
+    code: str = Field(
+        description="""Group code; the level of the group is determined based on
+        the length of  code. Each code contains between 1 and 4 digits."""
+    )
+    soc2020_group_title: str = Field(
+        default="",
+        description="""A short group title for the group code in SOC 2020.""",
+    )
+    group_description: str = Field(
+        default="", description="""More in-depth description of the group."""
+    )
+    qualifications: Optional[str] = Field(
+        default="",
+        description="""Typical
+        Entry Routes And Associated Qualifications for the unit group.""",
+    )
+    tasks: Optional[str] = Field(
+        default="",
+        description="""An optional string with a list of tasks for the group.
+        Applies only for Unit Group (4 digit code)""",
+    )
+
+    def pretty_print(self):
+        """Prints information associated with SOC code.
+
+        Returns:
+            str: string that contains all information regarding SOC code.
+        """
+        code = self.code
+        if len(code) < SOC_CODE_LENGTH:
+            out = "Code: " + code + " Title: " + self.soc2020_group_title + "."
+            if self.group_description:
+                out += " Description: " + self.group_description
+            if self.tasks:
+                out += " Tasks: " + self.tasks + "."
+        else:
+            out = ""
+        return out
---
+++ src/occupational_classification/meta/soc_meta.py
@@ -0,0 +1,112 @@
+"""Module for the 'SocDB' class and 'SocMeta' class.
+
+This module defines the 'SocDB' class, which makes necessary changes to the
+structure of the data being passed (soc_structure).
+This module defines the 'SocMeta' class, which retrieves titles and details
+for given SOC codes.
+"""
+
+import pandas as pd
+
+from occupational_classification.meta.classification_meta import ClassificationMeta
+
+
+class SocDB:
+    """Loads data from the config file.
+
+    Converts group level into one column, based on the length of the code.
+    Returns a list of dictionaries (use create_soc_dictionary method)
+    or a DataFrame (use create_soc_dataframe method).
+    """
+
+    def __init__(self, df):
+        self.df = df
+
+        # soc_meta = self.create_soc_dataframe()
+
+    def code_selection(self, soc_dict: dict) -> dict:
+        """Selects the meaningful 1, 2, 3, or 4 digit long code.
+            Aggregates into one column "code".
+
+        Returns:
+            Dictionary (dict) with not aggregated codes.
+        """
+        selected_key = next(
+            (
+                k
+                for k in [
+                    "soc2020_major_group",
+                    "soc2020_sub-major_group",
+                    "soc2020_minor_group",
+                    "soc_2020_unit_group",
+                ]
+                if soc_dict[k] != "<blank>"
+            ),
+            None,
+        )
+        cleaned_data = {k: v for k, v in soc_dict.items() if v != "<blank>"}
+        cleaned_data["code"] = cleaned_data.pop(selected_key)
+        return cleaned_data
+
+    def create_soc_dictionary(self) -> list:
+        """Iterates through the dataframe with SOC and converts to dictionaries.
+
+        Returns:
+            List of dictionaries, such as:
+                {"code": <code>,
+                "soc2020_group_title": <group_title>,
+                "group_description": <group_description>,
+                "qualifications": <entry_level_requirements_and_qualifications>,
+                "tasks": <tasks>}
+        """
+        soc_list = []
+        df = self.df
+
+        num_rows = len(self.code_selection(df.to_dict())["code"])
+
+        for row in range(num_rows):
+            soc_dict = self.code_selection(df.loc[row])
+            soc_validated = ClassificationMeta.model_validate(soc_dict)
+            soc_list.append(soc_validated.dict())
+        return soc_list
+
+    def create_soc_dataframe(self) -> pd.DataFrame:
+        """Takes a list of dictionaries and converts to a dataframe."""
+        return pd.DataFrame(SocDB(self).create_soc_dictionary())
+
+
+class SocMeta:
+    """SOC Meta data model class for SOC codes and their descriptions
+    build based on java dictionary from onsdigital repo.
+
+    Attributes:
+        soc_meta (List[ClassificationMeta]): List of ClassificationMeta objects
+    """
+
+    def __init__(self, df):
+        self.df = df
+        self.soc_meta = SocDB(df).create_soc_dictionary()
+
+    def get_meta_by_code(self, code: str) -> dict:
+        """Retrieve title and detail for a given SOC code.
+
+        Args:
+            code (str): A SOC code to lookup.
+
+        Returns:
+            dict: Dictionary with title and detail if found, else an error message.
+        """
+        for element in self.soc_meta:
+            if element["code"].startswith(code):
+                return {
+                    "code": element.get("code", None),
+                    "group title": element.get("soc2020_group_title", None),
+                    "group description": element.get("group_description", None),
+                    "typical entry routes and associated qualifications": element.get(
+                        "tasks", []
+                    ),
+                    "tasks": element.get("qualifications", []),
+                }
+
+        # No match found
+        return {"error": f"No metadata found for SOC code {code}"}
