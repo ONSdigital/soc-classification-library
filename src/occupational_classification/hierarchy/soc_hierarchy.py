6fb11a01448f7edb6b36572417c9ed48e3b2ce1b
peter-spencer-ons <peter.spencer@ext.ons.gov.uk>
2025-05-16 16:25:11 +0000

    chore(backend library): use soc_meta to get tasks and qualificaitons for SocNode

--- src/occupational_classification/hierarchy/soc_hierarchy.py
+++ src/occupational_classification/hierarchy/soc_hierarchy.py
@@ -250,7 +250,9 @@ def _define_codes_and_nodes(soc_df: pd.DataFrame):
     for code in soc_df["code"]:
         group_description = soc_meta.get_meta_by_code(code)["group description"]
         group_title = soc_meta.get_meta_by_code(code)["group title"]
-        soc_node = SocNode(code, group_title=group_title, group_description=group_description)
+        soc_node = SocNode(
+            code, group_title=group_title, group_description=group_description
+        )
 
         codes.append(code)
         nodes.append(soc_node)
@@ -267,16 +269,19 @@ def _populate_parent_child_relationships(nodes: list, code_node_dict: dict):
             code_node_dict[node.soc_code].parent = code_node_dict[parent_code]
 
 
-def _populate_tasks_and_quals(nodes: list, soc_df: pd.DataFrame):
+def _populate_tasks_and_quals(nodes: list):
     """Populate tasks and qualifications. Modifies nodes in places."""
+    soc_meta = SocMeta()
     for node in nodes:
-        if SocCode(node.soc_code).code_length() == _SOC_CODE_LENGTH:
-            qual = soc_df.loc[soc_df["code"] == node.soc_code, "qualifications"]
-            node.qualifications = qual.iloc[0]
+        code = node.soc_code
+        if SocCode(code).code_length() == _SOC_CODE_LENGTH:
+            qual = soc_meta.get_meta_by_code(code)[
+                "typical entry routes and associated qualifications"
+            ]
+            node.qualifications = qual
 
-            tasks = soc_df.loc[soc_df["code"] == node.soc_code, "tasks"]
-            tasks_list = tasks.iloc[0].replace("\n", "").split("~")
-
+            tasks = soc_meta.get_meta_by_code(code)["tasks"]
+            tasks_list = tasks.replace("\n", "").split("~")
             node.tasks = tasks_list[1:]
 
 
@@ -319,7 +324,7 @@ def load_hierarchy(soc_df, soc_index):
 
     _populate_parent_child_relationships(nodes, code_node_dict)
 
-    _populate_tasks_and_quals(nodes, soc_df)
+    _populate_tasks_and_quals(nodes)
 
     _populate_job_titles(nodes, soc_index)
 
